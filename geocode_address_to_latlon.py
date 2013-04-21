33c8af016dade5269347c983bad70e1d61941cd0
Mari Muraki <marimuraki@gmail.com>
2013-04-21 16:34:57 -0700

    added function to generate full addresses

--- geocode_address_to_latlon.py
+++ geocode_address_to_latlon.py
@@ -6,11 +6,19 @@ from csv import DictReader
 from sys import argv
 
 
-def readcsv(input_file):
+def read_csv(input_file):
     address_file = open(input_file, 'r')
     reader = DictReader(address_file)
     return reader
 
-
+def generate_addresses(reader):
+    addresses = (line for line in reader)
+    for address in addresses:
+        fulladdress = "{address}, {city}, {state}, {zip}".format(**address)
+        return fulladdress
+    
+    
 if __name__ == '__main__':
-    result = readcsv(argv[1])
+    readfile = read_csv(argv[1])
+    fulladdress = generate_addresses(readfile)
+
