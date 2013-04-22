e398630d1bcdbc92e5430af68c96caed23ee7e16
Mari Muraki <marimuraki@gmail.com>
2013-04-21 17:14:55 -0700

    added function to geocode addresses using geopy and Google V3

--- geocode_address_to_latlon.py
+++ geocode_address_to_latlon.py
@@ -3,6 +3,7 @@
 # using [address, city, state, zip] in CSV file
 
 from csv import DictReader
+from geopy import geocoders    
 from sys import argv
 
 
@@ -12,13 +13,24 @@ def read_csv(input_file):
     return reader
 
 def generate_addresses(reader):
+    address_list = []
     addresses = (line for line in reader)
     for address in addresses:
         fulladdress = "{address}, {city}, {state}, {zip}".format(**address)
-        return fulladdress
-    
-    
+        address_list.append(fulladdress)
+    return address_list
+        
+def geocode_addresses(address_list):
+    geocoded_list = []
+    g = geocoders.GoogleV3()
+    for fulladdress in address_list:
+        address, (lat, lon) = g.geocode(fulladdress)
+        geocoded = "%s: %.5f, %.5f" % (address, lat, lon)
+        geocoded_list.append(geocoded)
+    return geocoded_list
+        
 if __name__ == '__main__':
     readfile = read_csv(argv[1])
     fulladdress = generate_addresses(readfile)
+    geocode = geocode_addresses(fulladdress)
 
