bcff390cbf756a3dc7a87a8670a52925ecbb8b12
Mari Muraki <marimuraki@gmail.com>
2013-04-21 19:15:26 -0700

    added function to output geocoded addresses cleaned up code to a working version

--- geocode_address_to_latlon.py
+++ geocode_address_to_latlon.py
@@ -2,7 +2,7 @@
 # Geocode addresses to latitude/longitude, 
 # using [address, city, state, zip] in CSV file
 
-from csv import DictReader
+from csv import DictReader, DictWriter
 from geopy import geocoders    
 from sys import argv
 
@@ -10,27 +10,36 @@ from sys import argv
 def read_csv(input_file):
     address_file = open(input_file, 'r')
     reader = DictReader(address_file)
-    return reader
+    return list(reader)
 
-def generate_addresses(reader):
-    address_list = []
-    addresses = (line for line in reader)
-    for address in addresses:
-        fulladdress = "{address}, {city}, {state}, {zip}".format(**address)
-        address_list.append(fulladdress)
-    return address_list
+def generate_addresses(address_dicts):
+    for address_dict in address_dicts:
+        fulladdress = "{address}, {city}, {state}, {zip}" \
+            .format(**address_dict)
+        address_dict["fulladdress"] = fulladdress
+    return address_dicts
         
-def geocode_addresses(address_list):
-    geocoded_list = []
+def geocode_addresses(address_dicts):
     g = geocoders.GoogleV3()
-    for fulladdress in address_list:
-        address, (lat, lon) = g.geocode(fulladdress)
-        geocoded = "%s: %.5f, %.5f" % (address, lat, lon)
-        geocoded_list.append(geocoded)
-    return geocoded_list
-        
+    for address_dict in address_dicts:
+        address, (lat, lon) = g.geocode(address_dict["fulladdress"])
+        address_dict["latitude"] = lat
+        address_dict["longitude"] = lon
+    return address_dicts
+    
+def write_csv(output_file, address_dicts):
+    geocoded_file = open(output_file, 'wb')
+    writer = DictWriter(geocoded_file, fieldnames=address_dicts[0].keys(), \
+        dialect='excel', lineterminator='\n')
+    writer.writeheader()
+    writer.writerows(address_dicts)
+    geocoded_file.close() 
+
+
 if __name__ == '__main__':
-    readfile = read_csv(argv[1])
-    fulladdress = generate_addresses(readfile)
-    geocode = geocode_addresses(fulladdress)
+    addressdicts = read_csv(argv[1])
+    output_file = argv[2]
+    fulladdresses = generate_addresses(addressdicts)
+    geocoded = geocode_addresses(fulladdresses)
+    write_csv(output_file, geocoded)
 
